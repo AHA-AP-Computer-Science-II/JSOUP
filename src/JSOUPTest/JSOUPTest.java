9da541e0d86f9676c3717489d060e529ae4f15b3
Caleb Reiter <calreiter16@ahastars.org>
2016-02-08 11:30:53 -0600

    Changed JSOUP to jsoup

--- src/JSOUPTest/JSOUPTest.java
+++
@@ -1,5 +0,0 @@
-package JSOUPTest;
-
-public class JSOUPTest {
-
-}
---
+++ src/jsoupTest/jsoupTest.java
@@ -0,0 +1,11 @@
+package jsoupTest;
+
+public class jsoupTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
