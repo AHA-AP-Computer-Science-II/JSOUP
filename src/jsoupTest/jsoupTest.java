c18061e51ed4fcc9b595ef2e361edfb149a4ac4b
Caleb Reiter <calreiter16@ahastars.org>
2016-02-10 11:55:38 -0600

    test stuff

--- src/jsoupTest/jsoupTest.java
+++ src/jsoupTest/jsoupTest.java
@@ -1,8 +1,26 @@
 package jsoupTest;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class jsoupTest {
 
 	public static void main(String[] args) {
+		
+//		Document doc = new Document("http://www.google.com");
+		
+		Document doc;
+		
+		try {
+			doc = Jsoup.connect("http://www.google.com").get();
+			Element body = doc.body();
+			System.out.println(body);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 		
